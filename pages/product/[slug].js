fdd7683d6dc6aa6ccdd752ef14316cd04dde3d22
ravenvsteam3@riasolutionsmspartner.onmicrosoft.com <ravenvsteam3@riasolutionsmspartner.onmicrosoft.com>
2023-05-22 17:12:07 +0300

    Changing wrong spelling

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -83,7 +83,7 @@ const ProductDetails = ({ product, products }) => {
 
             <div className='maylike-products-wrapper'>
                 <h2>You may also like</h2>
-                <div className='markquee'>
+                <div className='marquee'>
                     <div className='maylike-products-container track'>
                         {products.map((item) =>
                             <Product key={item._id}
